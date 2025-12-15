af8943459a4145499ec9a769585b322f0c907096
Gabriel Rosa <control.brosa07@gmail.com>
2025-12-15 00:47:49 -0300

    chore: add protected route

--- app/_components/AppLayout.tsx
+++ app/_components/AppLayout.tsx
@@ -1,16 +1,19 @@
 "use client";
 
 import { AppSidebar } from "@/app/_components/AppSiderbar";
+import ProtectedRoute from "@/app/_components/auth/ProtectedRoute";
 import { SidebarInset, SidebarProvider } from "@/app/_components/ui/sidebar";
 import { ReactNode } from "react";
 
 export function AppLayout({ children }: { children: ReactNode }) {
   return (
-    <SidebarProvider>
-      <AppSidebar />
-      <SidebarInset>
-        <main className="p-6">{children}</main>
-      </SidebarInset>
-    </SidebarProvider>
+    <ProtectedRoute>
+      <SidebarProvider>
+        <AppSidebar />
+        <SidebarInset>
+          <main className="p-6">{children}</main>
+        </SidebarInset>
+      </SidebarProvider>
+    </ProtectedRoute>
   );
 }
